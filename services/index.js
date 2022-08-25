dbdb2a58e0a8304ccce3f01459f9b5a814f5ce13
Ranjit Singh <ranjeetghotra6@gmail.com>
2022-08-25 09:58:32 -0700

    Added logger
    Added logger

--- services/index.js
+++ services/index.js
@@ -1,8 +1,10 @@
 import DBoperations from './DBoperations';
 import commonFunctions from './commonFunctions';
 import scheduler from './scheduler';
+import logger from './logger';
 
 module.exports = {
+    logger,
     DbOperations : DBoperations,
     commonFunctions: commonFunctions,
     scheduler:scheduler
